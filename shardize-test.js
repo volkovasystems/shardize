26fe2a169e262f8d404b7411567cb01b70e84faa
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-09 12:44:33 +0800

    Updated test.

--- shardize-test.js
+++ shardize-test.js
@@ -2,12 +2,12 @@
 const assert = require( "assert" );
 const shardize = require( "./shardize.js" );
 
-assert.equal( shardize( "Hello World" ), "hello-world", "should have value 'hello-world'" );
+assert.equal( shardize( "Hello World" ), "hello-world", "should be equal to 'hello-world'" );
 
-assert.equal( shardize( "hello_world" ), "hello-world", "should have value 'hello-world'" );
+assert.equal( shardize( "hello_world" ), "hello-world", "should be equal to 'hello-world'" );
 
-assert.equal( shardize( "helloWorld" ), "hello-world", "should have value 'hello-world'" );
+assert.equal( shardize( "helloWorld" ), "hello-world", "should be equal to 'hello-world'" );
 
-assert.equal( shardize( "hello world" ), "hello-world", "should have value 'hello-world'" );
+assert.equal( shardize( "hello world" ), "hello-world", "should be equal to 'hello-world'" );
 
 console.log( "ok" );
